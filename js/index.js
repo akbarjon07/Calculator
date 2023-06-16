db4bd551e166c3bcc5c93f01a0bb91c80e11e6bd
akbarjon07 <akbarjon.1019@gmail.com>
2023-06-16 14:11:53 +0500

    bugs fixed

--- js/index.js
+++ js/index.js
@@ -1,28 +1,68 @@
 const input = document.querySelector(".calculator__input");
 let buttons = document.querySelectorAll("button");
-let result = [];
-let calculation
 
-
-const calculate = (button) => {
-    const value = button.textContent;
-    if (value === "RESET") {
-        result = [];
-        input.textContent = '0'
-    } else if (value === "=") {
-        input.textContent = eval(calculation)
-    } else if (value === "DEL") {
-        calculation = calculation.toString().slice(0, -1);
-        input.textContent = calculation;
+function calculate(value) {
+    const calculatedValue = eval(value  || null);
+    if (isNaN(calculatedValue)) {
+        input.value = "Error";
     } else {
-        result.push(value);
-        calculation = result.join('');
-        input.innerHTML = calculation;
+        input.value = calculatedValue;
     }
 }
 
+document.addEventListener("click", keyboardInputHandler);
+function keyboardInputHandler(e) {
+  e.preventDefault();
 
 
-buttons.forEach(button => {
-    button.addEventListener("click", () => calculate(button))
-})
+  if (e.target.innerHTML === "0") {
+    input.value += "0";
+  } else if (e.target.innerHTML === "1") {
+    input.value += "1";
+  } else if (e.target.innerHTML === "2") {
+    input.value += "2";
+  } else if (e.target.innerHTML === "3") {
+    input.value += "3";
+  } else if (e.target.innerHTML === "4") {
+    input.value += "4";
+  } else if (e.target.innerHTML === "5") {
+    input.value += "5";
+  } else if (e.target.innerHTML === "6") {
+    input.value += "6";
+  } else if (e.target.innerHTML === "7") {
+    input.value += "7";
+  } else if (e.target.innerHTML === "7") {
+    input.value += "7";
+  } else if (e.target.innerHTML === "8") {
+    input.value += "8";
+  } else if (e.target.innerHTML === "9") {
+    input.value += "9";
+  }
+
+  if (e.target.innerHTML === "+") {
+    input.value += "+";
+  } else if (e.target.innerHTML === "-") {
+    input.value += "-";
+  } else if (e.target.innerHTML === "*") {
+    input.value += "*";
+  } else if (e.target.innerHTML === "/") {
+    input.value += "/";
+  }
+
+  if (e.target.innerHTML === ".") {
+    input.value += ".";
+  }
+
+  if (e.target.innerHTML === "=") {
+    calculate(input.value);
+  }
+
+  if (e.target.innerHTML === "DEL") {
+    const resultInput = input.value;
+    input.value = resultInput.substring(0, input.value.length - 1);
+  }
+
+  if (e.target.innerHTML === "RESET") {
+    input.value = "";
+  }
+}
