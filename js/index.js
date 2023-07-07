8e4788de72d82780a102501982c76d64e401667e
akbarjon07 <akbarjon.1019@gmail.com>
2023-07-07 09:34:45 +0500

    bug is find

--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@ const input = document.querySelector(".calculator__input");
 let buttons = document.querySelectorAll("button");
 
 function calculate(value) {
-    const calculatedValue = eval(value  || null);
-    if (isNaN(calculatedValue)) {
-        input.value = "Error";
-    } else {
-        input.value = calculatedValue;
-    }
+  const calculatedValue = eval(value  || null);
+  if (isNaN(calculatedValue)) {
+    input.value = "Error";
+  } else {
+    input.value = calculatedValue;
+  }
 }
 
 
@@ -20,7 +20,7 @@ function keyboardInputHandler(e) {
   } else if (e.target.innerHTML === "1") {
     input.value += "1";
   } else if (e.target.innerHTML === "2") {
-    input.value += "2";  
+    input.value += "2";
   } else if (e.target.innerHTML === "3") {
     input.value += "3";
   } else if (e.target.innerHTML === "4") {
@@ -56,17 +56,88 @@ function keyboardInputHandler(e) {
   }
 }
 
+
+// THIS IS MY ISSUE
+
 const operator = document.querySelectorAll(".operator");
 
-for (let item of operator) {
-  item.addEventListener("click", e => {
-    if (e.target.innerHTML === "+" || e.target.textContent === "-" || e.target.textContent === "*" || e.target.textContent === "/") {
-      input.value += ""
-      input.value += e.target.innerHTML;
-      console.log(input.value);
+operator.forEach((op) => {
+  op.addEventListener("click", e => {
+    let actions = []
+    let oper = e.target.innerHTML;
+    actions.push(oper)
+    let lastPressed = null;
+    for (const action of actions) {
+      if (lastPressed === null) {
+        lastPressed = action;
+        // console.log(lastPressed);
+        input.value =+ lastPressed.innerHTML;
+      } else if (action > lastPressed) {
+        lastPressed = action;
+        // console.log(lastPressed);
+        input.value =+ lastPressed.innerHTML;
+      }
     }
+
   })
-}
+});
+
+
+// const lastChar = input.value.slice(-1);
+// input.value =+ lastChar
+// function lastPressed(actions) {
+//   let lastPressed = null;
+//   for (const action of actions) {
+//     if (lastPressed === null) {
+//       lastPressed = action;
+//     } else if (action > lastPressed) {
+//       lastPressed = action;
+//     }
+//   }
+//   return lastPressed;
+// }
+
+
+// При нажатии одного из операторов проверяйте последний символ инпута. Если он - тоже оператор, удаляйте его из строки и добавляйте новый оператор.
+
+//////////////////////////////////////////////////////////////////////
+
+// const operator = document.querySelectorAll(".operator");
+
+// operator.forEach((op) => {
+//   op.addEventListener("click", e => {
+//     // let buttonText = e.target.textContent;
+//     let opers = [];
+//     opers.push(e.target.textContent)
+//     console.log(opers);
+//     console.log(opers.length);
+//     if (opers.length >= 1) {
+//       let res = opers.pop();
+//       input.value += res;
+//     }
+//     else {
+//       input.value += '';
+//     }
+
+//   })
+// });
+
+//////////////////////////////////////////////////////////////////////
+
+// const operator = document.querySelectorAll(".operator");
+
+// for (let item of operator) {
+//   item.addEventListener("click", e => {
+//     if (e.target.innerHTML === "+" || e.target.textContent === "-" || e.target.textContent === "*" || e.target.textContent === "/") {
+//       input.value += e.target.innerHTML;
+//     }
+//   })
+// }
+
+
+//////////////////////////////////////////////////////////////////////
+
+// const operator = document.querySelectorAll(".operator");
 
 // for (let i = 0; i < operator.length; i++) {
 //   operator[i].addEventListener("click", function(event) {
